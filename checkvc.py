f1dd087c05b7ebff012e96ffb129c6b54d30b4fc
Conshmea <77846041+Conshmea@users.noreply.github.com>
2022-04-24 15:14:33 +0100

    Update checkvc.py
    commented it

--- checkvc.py
+++ checkvc.py
@@ -6,24 +6,26 @@ class CheckVc(commands.Cog):
         self.bot = bot
         self.Vcs = {}
 
-        print(bot.hi)
-
+    #runs on user joining or leaving VC
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
         print(member, before.channel, after.channel)
+        
+        #When user joins or leaves VC add or remove them from that VCs list. If there is no list create one
         try:
-            if before.channel == None:
+            if after.channel != None:
                 self.Vcs[str(after.channel)].append(f"{member.name}#{member.discriminator}")
             else:
                 self.Vcs[str(before.channel)].remove(f"{member.name}#{member.discriminator}")
         except KeyError:
-            if before.channel == None:
+            if after.channel != None:
                 self.Vcs[str(after.channel)] = []
                 self.Vcs[str(after.channel)].append(f"{member.name}#{member.discriminator}")
             else:
                 self.Vcs[str(before.channel)] = []
                 self.Vcs[str(before.channel)].remove(f"{member.name}#{member.discriminator}")
 
+        #If it is the last user to leave the VC delete and clear the logged messagesfor the VC
         if before.channel != None:
             print(self.bot.VC_messages[before.channel.id])
             if self.Vcs[str(before.channel)] == []:
